bfd44ca803487309727fbad93ff012e5626d52dc
Chibytez <aniakuchibuike@gmail.com>
2019-02-15 00:22:13 +0100

    adding user create request

--- SERVER/Controller/userControllers.js
+++ SERVER/Controller/userControllers.js
@@ -27,3 +27,14 @@ export const getRequestById =(req, res) => {
     if (!request) return res.status(404).send('The request with the given ID was not found.');
     res.send(request);
 };
+
+export const createRequest =(req, res) => {
+    const request = {
+        id: requests.length + 1,
+        requesting: req.body.requesting,
+        name: req.body.name,
+        email: req.body.email
+    }
+    requests.push(request);
+    res.send(request);
+};
--- SERVER/routes/userRoutes.js
+++ SERVER/routes/userRoutes.js
@@ -1,10 +1,12 @@
     import {getAllRequest, 
-    getRequestById, 
+    getRequestById,
+    createRequest 
 } from '../Controller/userControllers';
 
 const userRoutes = (app) => {
-    app.get('/api/v1/requests', getAllRequest );
-    app.get('/api/v1/requests/:id', getRequestById)
+    app.get('/api/v1/users/requests', getAllRequest );
+    app.get('/api/v1/users/requests/:id', getRequestById);
+    app.post('/api/v1/users/requests', createRequest)
 }
 
     export default userRoutes;
