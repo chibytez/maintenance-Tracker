e26435c0eccd330df96911470579c9774a4db9dc
Chibytez <aniakuchibuike@gmail.com>
2019-03-17 11:22:32 +0100

    adding users get all request

--- SERVER/routes/userRoutes.js
+++ SERVER/routes/userRoutes.js
@@ -1,16 +1,16 @@
-    import {getAllRequest, 
-    getRequestById,
-    createRequest,
-    modifyRequest,
-    cancelRequest
+    import {getAllUsersRequests, 
+    // getRequestById,
+    // createRequest,
+    // modifyRequest,
+    // cancelRequest
 } from '../Controller/requestController';
 
 const userRoutes = (app) => {
-    app.get('/api/v1/users/requests', getAllRequest );
-    app.get('/api/v1/users/requests/:id', getRequestById);
-    app.post('/api/v1/users/requests', createRequest);
-    app.put('/api/v1/users/requests/:id',modifyRequest);
-    app.delete('/api/v1/users/requests/:id', cancelRequest)
-}
+    app.get('/api/v1/users/requests', getAllUsersRequests );
+//     app.get('/api/v1/users/requests/:id', getRequestById);
+//     app.post('/api/v1/users/requests', createRequest);
+//     app.put('/api/v1/users/requests/:id',modifyRequest);
+//     app.delete('/api/v1/users/requests/:id', cancelRequest)
+ }
 
     export default userRoutes;
